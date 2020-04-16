3f94af8aa38dbc1fe5d40e92291ec7487136d957
katsura_jp <nok.addayo@gmail.com>
2020-04-16 22:12:09 +0900

    [add] tensorboard

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import logging
 log = logging.getLogger(__name__)
 
 import torch
+from torch.utils.tensorboard import SummaryWriter
 import hydra
 from omegaconf import DictConfig
 from torch.optim import *
@@ -13,21 +14,22 @@ from src.fed_zoo import *
 from src.utils import *
 from src.utils.data import get_mnist_data
 
+
 @hydra.main(config_path="./config/config.yaml", strict=True)
 def main(cfg: DictConfig):
     os.chdir(cfg.root)
     log.info("\n" + cfg.pretty())
 
     model = eval(cfg.model.classname)(**cfg.model.args)
-
-    federater = eval(cfg.fed.classname)(
-                        model=model,
-                        optimizer=eval(cfg.optim.classname),
-                        optimizer_args=cfg.optim.args,
-                        num_clients=cfg.K,
-                        batchsize=cfg.B,
-                        fraction=cfg.C,
-                        local_epoch=cfg.E)
+    writer = SummaryWriter(log_dir=os.path.join(cfg.savedir, "tf"))
+    federater = eval(cfg.fed.classname)(model=model,
+                                        optimizer=eval(cfg.optim.classname),
+                                        optimizer_args=cfg.optim.args,
+                                        num_clients=cfg.K,
+                                        batchsize=cfg.B,
+                                        fraction=cfg.C,
+                                        local_epoch=cfg.E,
+                                        writer=writer)
 
     federater.fit(cfg.n_round)
     pass
